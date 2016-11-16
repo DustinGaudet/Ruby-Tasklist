13aca9ac9f492dca977e3801a68cbdbe1daf48ff
Dustin Gaudet <dustin@dustingaudet.com>
2016-11-16 17:00:33 -0500

    Give lists the ability to add tasks via an add method.

--- todo.rb
+++ todo.rb
@@ -1,6 +1,18 @@
 # Classes
 
-class List; end
+class List
+  attr_reader :all_tasks
+
+  def initialize()
+    @all_tasks = []
+  end
+
+  public
+  def add(task)
+    all_tasks << task
+  end
+end
+
 class Task 
   attr_reader :description
 
@@ -12,4 +24,6 @@ end
 if __FILE__ == $PROGRAM_NAME
   list_1 = List.new
   puts "New list created!"
+  list_1.add(Task.new("Walk the dog."))
+  puts "You've added a task to the list!"
 end
