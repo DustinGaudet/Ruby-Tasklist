1e6d575f6ba577dd1b342a44330f3cba2a00c746
Dustin Gaudet <dustin@dustingaudet.com>
2016-11-18 13:04:40 -0500

    Add list instance method for showing the tasks in a list.

--- todo.rb
+++ todo.rb
@@ -3,13 +3,18 @@
 class List
   attr_reader :all_tasks
 
-  def initialize()
+  def initialize
     @all_tasks = []
   end
 
   public
   def add(task)
     all_tasks << task
+  end
+  def show_tasks
+  	all_tasks.each do |task| 
+  		puts task.description
+  	end
   end
 end
 
@@ -25,5 +30,8 @@ if __FILE__ == $PROGRAM_NAME
   list_1 = List.new
   puts "New list created!"
   list_1.add(Task.new("Walk the dog."))
-  puts "You've added a task to the list!"
+  list_1.add(Task.new("Walk the cat."))
+  list_1.add(Task.new("Walk the bear."))
+  puts "You've added 3 tasks to the list!"
+  list_1.show_tasks
 end
